7e8aebd3c2e1f46d525b1fba8576d47e0284976e
smart <smart@open-neurosecurity.org>
2023-12-05 10:16:54 -0500

    Google analytics

--- wp-content/themes/journal/header.php
+++ wp-content/themes/journal/header.php
@@ -7,7 +7,15 @@
 wp_head(); 
 ?>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QZJECS14WM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-QZJECS14WM');
+</script>
 <body class="home blog hfeed has-header-image has-sidebar colors-dark">
 	<header id="site-header">
 
