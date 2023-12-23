43dc78170a69b2f384b11b7d9beeb6e1d292a8e6
smart <smart@open-neurosecurity.org>
2023-12-23 07:53:25 -0500

    webhooks.php fixes

--- webhooks.php
+++ webhooks.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL|E_STRICT);
+error_reporting(E_ALL);
 require_once dirname(__FILE__) . '/wp-load.php';
 require_once dirname(__FILE__) . '/wp-includes/post.php';
 
@@ -29,25 +29,27 @@ function update_post($data = array(), $options = array())
 
 
 function add_vote_to_post($post_id, $redis) {
-	//global $redis;
-	//$redis->connect('127.0.0.1', 6379);
-	var_dump($redis);
-	$remoteip = '127.0.0.1';
+	#global $redis;
+	#$redis->connect('127.0.0.1', 6379);
+	$json = json_encode(array('code' => '201'));
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' 
+		&& isset($_SERVER['X_REAL_IP'])) {
+	$remoteip = $_SERVER['X_REAL_IP'];
 	$voted = false;
 	if (! $redis->sIsMember('clients', $remoteip)){
 		$post = get_post($post_id);
-		$c = intval($post->likes_count) + 1; # n+1
-		$voted = update_post(array('id' => $post_id, 'likes_count' => $c)); 	
-	} 	
-	if ($voted == true) {
-		$json = json_encode(array("result" => "$post->likes_count", 
+		$count = intval($post['likes_count']) + 1; # n+1
+		update_post(array('id' => $post_id, 'likes_count' => $count)); 	
+		$voted = true;
+		$json = json_encode(array("result" => $post['likes_count'], 
 			"success" => true, "code" => 200));
 		$redis->sAdd('clients', $remoteip);
 	} else {
 		$json = json_encode(array("success" => false, "code" => 403));
 	}
 	$redis->close(); #Close redis connection. 
-	return json_decode($json);
+	}
+	return $json;
 }
 
 
@@ -55,15 +57,14 @@ function add_vote_to_post($post_id, $redis) {
 if (! empty($_GET['id'])) {
 	$post_id = strval($_GET['id']);
 	$json = add_vote_to_post($post_id, REDIS_CLIENT);
-	#var_dump($json);
-	if ($json->code == 200) {
+	if (isset($json['code']) && intval($json['code']) == 201) {
 		header('Content-Type: application/json; charset=utf-8');
 		http_response_code(200);
 		echo $json;
 	} else {
 		header('Content-Type: text/html; charset=utf-8');
-		http_response_code($json->code);
-		echo sprintf("<p>Permission denied. (%s)</p>", $json->code);
+		http_response_code(500);
+		echo "<p>Internal server error</p>";
 	}
 } else {
 	# return error response 400
