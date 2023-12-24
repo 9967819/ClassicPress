c06ed1587621c706ac8ac628dd0e3634c63d1959
smart <smart@open-neurosecurity.org>
2023-12-24 16:40:47 -0500

    test

--- wp-content/themes/journal-winter/index.php
+++ wp-content/themes/journal-winter/index.php
@@ -6,10 +6,12 @@ get_header();
 <div id="content" class="site-main">
 			
 			<!-- youtube 2024 embedded playlist -->
-			<article class="video grayscale">
-			<p>Joyeuses fêtes à tous!
+			<article class="video">
+			<p>Joyeuses fêtes à tous et à toutes!
 			<i class="red fa-regular fa-face-kiss-wink-heart"></i></p>
-			<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?si=E4K8xNvFTwbAF0X8&amp;list=PL7Ndnn1jFxfvUyTnLYhJaexNtFUJiTqQZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			<!-- <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?si=E4K8xNvFTwbAF0X8&amp;list=PL7Ndnn1jFxfvUyTnLYhJaexNtFUJiTqQZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			-->
+			<img src="https://open-neurosecurity.org/wp-content/uploads/2023/12/meandsergiodec2023.png" alt="#">
 			</article>
 	
 			<?php
