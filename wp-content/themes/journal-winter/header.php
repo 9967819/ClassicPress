5ed6f8b162c1713aaa4a52ebf0f8e46dcf798a4b
smart <smart@open-neurosecurity.org>
2024-01-06 16:48:12 -0500

    metadata

--- wp-content/themes/journal-winter/header.php
+++ wp-content/themes/journal-winter/header.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" prefix="og: https://ogp.me/ns#">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php 
 wp_head(); 
 ?>
+<meta name="description" content="La neurosécurité libre et ouverte pour tous.">
+<meta name="keywords" content="neurosecurity, neurosécurité, cyberdépendance, applied human neurosecurity journal, applied neurosecurity, free neurosecurity">
+<meta property="og:author" content="Applied Human Neurosecurity Journal">
+<meta property="og:image:url" content="https://open-neurosecurity.org/static/img/logo-ahnjournal-v2.png">
+<meta property="og:image:secure_url" content="https://open-neurosecurity.org/static/img/logo-ahnjournal-v2.png">
+<meta property="og:image:alt" content="site image">
+<script type="application/ld+json">
+{"@context":"https://schema.org",
+	"@type":"Periodical",
+	"@id":"https://open-neurosecurity.org",
+	"name":"Applied Human Neurosecurity Journal",
+	"datePublished":"1753-01-01",
+	"url":"https://open-neurosecurity.org",
+	"inLanguage":"fr",
+	"copyrightHolder":"Robillard, Etienne",
+	"copyrightYear":"2024",
+	"publisher":"Applied Human Neurosecurity Journal",
+	"description":"Open-access publisher focusing on next-generation neurosecurity research, education and advocacy.",
+	"siteName":"AHNJournal",
+	"siteNameTitle":"Applied Human Neurosecurity Journal"}
+</script>
+<link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="/wp-content/themes/journal-winter/assets/fontawesome/css/fontawesome.min.css">
 <link rel="stylesheet" href="/wp-content/themes/journal-winter/assets/fontawesome/css/brands.css">
 <link rel="stylesheet" href="/wp-content/themes/journal-winter/assets/fontawesome/css/solid.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&display=swap">
 </head>
 <body>
-	<header id="site-header">
-
-		<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-	</header>
+<header id="site-header">
+<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
+</header>
