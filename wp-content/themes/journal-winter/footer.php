2e2f8330dae00803963cb0bebb6cde3c63a1bfba
smart <smart@open-neurosecurity.org>
2024-01-08 16:18:21 -0500

    snowflakes

--- wp-content/themes/journal-winter/footer.php
+++ wp-content/themes/journal-winter/footer.php
@@ -1,5 +1,5 @@
 </div>
-<footer class="site-footer overlay">
+<footer class="site-footer overlay" id="footer-snowflakes">
 	   <section class="align-items-center row">
 		<?php
 			get_template_part( 'template-parts/footer/footer', 'widgets' );
@@ -11,7 +11,8 @@
 </footer>
 <!-- Google tag (gtag.js) -->
 <script async cross-origin="anonymous" src="https://www.googletagmanager.com/gtag/js?id=G-QZJECS14WM"></script>
-<script>
+			<script>
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
@@ -22,5 +23,24 @@ gtag('config', 'G-QZJECS14WM');
 <script async src="/wp-content/themes/journal-winter/assets/js/widgets.js"></script>
 
 
+<script src="/static/snowflakes.js"></script>
+ <script>
+        var snowflakes = new Snowflakes({
+            color: '#fff', // Default: "#5ECDEF"
+            container: document.querySelector('footer'), // Default: document.body
+            count: 100, // 100 snowflakes. Default: 50
+            minOpacity: 0.1, // From 0 to 1. Default: 0.6
+            maxOpacity: 0.95, // From 0 to 1. Default: 1
+            minSize: 4, // Default: 10
+            maxSize: 10, // Default: 25
+            rotation: true, // Default: true
+            speed: 2, // The property affects the speed of falling. Default: 1
+            wind: false, // Without wind. Default: true
+            //width: 500, // Default: width of container
+            //height: 250, // Default: height of container
+            zIndex: 100, // Default: 9999,
+            autoResize: false // Default: true
+        });
+    </script>
 </body>
 </html>
