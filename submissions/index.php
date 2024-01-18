3c38b75af8895afcdb3e7a8e1dddb91b77eefb88
smart <smart@open-neurosecurity.org>
2024-01-18 00:14:56 -0500

    test

--- submissions/index.php
+++ submissions/index.php
@@ -6,25 +6,31 @@ get_header();
 ?>
 
 <div class="wrap">
-	<main class="site-main" id="content">
+	<div id="content" class="primary">
 		<h2 class="page-title">Directives aux auteurs</h2>
 		
-		<form method="POST" action=".">
-		<label for="first_name">Entrez votre prénom: </label>
-		<input type="text" name="first_name" id="first_name" required />
-		<label for="last_name">Entrez votre nom de famille: </label>
-		<input type="text" name="last_name" id="last_name" required />
-		<label for="email">Entrez votre courriel: </label>
-		<input type="email" name="email" id="email" required />
+		<div class="form">
+		<form method="POST" action="">
+		<label for="first_name">Prénom de l'auteur: </label>
+		<input type="text" name="first_name" id="first_name" required>
+		<label for="last_name">Nom de famille: </label>
+		<input type="text" name="last_name" id="last_name" required>
+		<label for="email">Courriel: </label>
+		<input type="email" name="email" id="email" required>
 		<label for="title">Titre du manuscript: </label>
-		<input type="text" id="title" required />
-		<label for="abstract">Description courte de l'article</label>
-		<input type="text" name="abstract" required />
+		<input type="text" id="title" required>
+		<hr>
+		<label for="category">Categorie: </label>
+		<select></select>
+		<label for="abstract">Abstract: </label>
+		<input type="text" name="abstract" required>
 		<label for="submission_file">Ajouter un document</label>
-		<input type="file" name="submission_file" required />
-		<input type="submit" value="Envoi"/>		
-	    </form>
-	</main>
+		<input type="file" name="submission_file" required>
+		<hr>
+		<input type="button" class="menu-label" value="Next">		
+		</form>
+		</div>
+	</div>
 </div>
 <?php
 get_footer();
