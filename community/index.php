0fdf43b90fbd8cc0e84a581fe5bbe5462afe5bcb
smart <smart@open-neurosecurity.org>
2024-01-23 13:54:16 -0500

    blndfln

--- community/index.php
+++ community/index.php
@@ -3,26 +3,27 @@ define('WP_USE_THEMES', true);
 require_once(dirname(__FILE__) . '/../wp-load.php');
 require_once(dirname(__FILE__) . '/../wp-includes/class-simplepie.php');
 get_header();
+$rss = fetch_feed('https://open-neurosecurity.org/forum/index.php?action=.xml;type=rss2'); 
 ?>
 
 <div class="wrap">
-	<div class="site-header">
-		<h2 class="page-title">Communauté</h2>
-	</div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main entry-content">
-			<h3 style="color: #fff"></h3>
-			<?php 
-$rss = fetch_feed('https://open-neurosecurity.org/forum/index.php?action=.xml;type=rss2'); 
-?>
+		<h2 class="page-title">Communauté</h2>
+		<section>
+			<h3>Now playing....</h3>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=_B8h9MSJGJmq8jGu&amp;list=PL7Ndnn1jFxfuu2Kmuk7wvaq5j_onbUXpf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+		<section>
+<section>
+<h3>Latest Forum Posts</h3>
+<ul>
 <?php foreach($rss->get_items(0, 5) as $item): ?>
-<div class="item">
-	<h4 class="title"><a href="<?php echo $item->get_permalink(); ?>">
-<?php echo $item->get_title(); ?></a></h4>
-</div>
+	<li><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></li>
 <?php endforeach; ?>
-
-			<h3 style="color: #fff">Modifications récentes</h3>
+</ul>
+</section>
+<section>
+<h3>Contributions récentes</h3>
 <?php
 $url = 'https://open-neurosecurity.org/wiki/api.php?hidebots=1&urlversion=1&days=7&limit=50&action=feedrecentchanges&feedformat=atom';
 $rss2 = fetch_feed($url);
@@ -33,6 +34,7 @@ $rss2 = fetch_feed($url);
 <?php echo $var2->get_title(); ?></a></h4>
 </div>
 <?php endforeach; ?>
+</section>
 </main>
 </div>
 </div>
