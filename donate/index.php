c739a0b518d9b5905065327f57057fb146a21667
Etienne Robillard <smart@open-neurosecurity.org>
2024-01-29 10:18:16 -0500

    testing phase 2

--- donate/index.php
+++ donate/index.php
@@ -3,40 +3,52 @@ define('WP_USE_THEMES', true);
 require_once(dirname(__FILE__) . '/../wp-load.php');
 require_once(dirname(__FILE__) . '/../wp-includes/class-simplepie.php');
 get_header();
-$rss = fetch_feed('https://open-neurosecurity.org/forum/index.php?action=.xml;type=rss2'); 
+$ssl_cipher = $_SERVER['CLIENT_SSL_CIPHER'];
 ?>
 <div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main entry-content">
-		<h2>Faire un don à Applied Human Neurosecurity Journal</h2>
+<div id="primary" class="content-area">
+<main id="main" class="site-main entry-content">
+<h2>Faire un don à Applied Human Neurosecurity Journal</h2>
 <div id="checkout-console" class="overlay1">
-Choisir un mode de paiement: 
-
-<button type="button" class="button btn-medium"><a href="https://www.paypal.com/paypalme/EtienneRobillard">PayPal</a></button>
+<label>Choisir un mode de paiement :</label>
+<div id="checkout-google"></div>
+<p>Merci d'encourager Applied Human Neurosecurity Journal.</p>
 </div>
-    </main>
-	</div>
+</main>
+</div>
 </div>
 <!-- Google Wallet client/api -->
-<script async src="https://pay.google.com/gp/p/js/pay.js"></script>
+<script async defer src="https://pay.google.com/gp/p/js/pay.js"></script>
 <script src="/static/assets/google/google.js"></script>
-
 <script>
-const paymentsClient = new google.payments.api.PaymentsClient({environment: 'TEST'});
-paymentsClient.isReadyToPay(isReadyToPayRequest)
-    .then(function(response) {
-if (response.result) {
-				const googleBtn =
-					paymentsClient.createButton({
-						onClick: () => console.log('TODO: click handler'),
-    				allowedPaymentMethods: []}); // same payment methods as for the loadPaymentData() API call
-				document.getElementById('checkout-console').appendChild(googleBtn);
-			}
-    })
-    .catch(function(err) {
+window.addEventListener("load", (event) => {
+
+    if(isReadyToPayRequest){
+	    const paymentsClient = new google.payments.api.PaymentsClient({environment: 'TEST'});
+	    paymentsClient.isReadyToPay(isReadyToPayRequest).then(function(response) {
+	    if (response.result) {
+			const googleBtn = paymentsClient.createButton({
+            onClick: () => {
+              paymentsClient.loadPaymentData(paymentDataRequest).then(function(paymentData){
+              // if using gateway tokenization, pass this token without modification
+              paymentToken = paymentData.paymentMethodData.tokenizationData.token;
+            }).catch(function(err){
+              // show error in developer console for debugging
+              console.error('hmm problem :' + err);
+            });
+            },
+    		allowedPaymentMethods: []}); // same payment methods as for the loadPaymentData() API call
+			document.getElementById('checkout-google').appendChild(googleBtn);
+		}
+      }).catch(function(err) {
       // show error in developer console for debugging
-      console.error(err);
-    });
+      console.log('good morning smart');
+      console.log(err);
+      });
+    } else {
+      console.log('problem with isReadyToPayRequest');
+    }    
+});//add event listener
 </script>
 <?php
 get_footer();
